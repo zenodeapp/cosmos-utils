273e4f4f1a2627e9388d1d3be0649bc6e6e29bc7
ZENODE <zenodeapp@gmail.com>
2024-01-15 12:03:48 +0100

    Update restate-sync.sh

--- tools/restate-sync.sh
+++ tools/restate-sync.sh
@@ -24,9 +24,15 @@ EXTERNAL_REPO=https://raw.githubusercontent.com/zenodeapp/restate-sync/$VERSION
 # restate-sync.sh
 # See https://github.com/zenodeapp/restate-sync/blob/v1.0.0/restate-sync.sh
 wget -O $SAVE_AS $EXTERNAL_REPO/restate-sync.sh
-if sh $SAVE_AS "$BINARY_NAME" "$NODE_DIR_NAME" "$1" "$2" "$3"; then
-    rm $SAVE_AS
-else
-    rm $SAVE_AS
-    exit 1
-fi
+
+# Execute script
+sh $SAVE_AS "$BINARY_NAME" "$NODE_DIR_NAME" "$1" "$2" "$3"
+
+# Capture the exit status of the script execution
+RESULT=$?
+
+# Cleanup
+rm $SAVE_AS
+
+# Exit the script with the captured exit status, indicating success (0) or failure (non-zero)
+exit $RESULT
