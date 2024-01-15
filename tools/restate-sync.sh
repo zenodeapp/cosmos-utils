3e1907b108cd566e0b48e8f3e04ffd10adcd6583
ZENODE <zenodeapp@gmail.com>
2024-01-15 11:53:08 +0100

    Update restate-sync.sh

--- tools/restate-sync.sh
+++ tools/restate-sync.sh
@@ -8,8 +8,14 @@
 # $2 is [rpc_server_1] (default: the first rpc configured in rpc_servers)
 # $3 is [rpc_server_2] (default: [rpc_server_1])
 
+# Root of repository
+ROOT=$(cd "$(dirname "$0")"/.. && pwd)
+
 # Source the _variables.sh file
-. "$(cd "$(dirname "$0")"/.. && pwd)/_variables.sh"
+. "$ROOT/_variables.sh"
+
+# Where the restate-sync.sh file has to be saved.
+SAVE_AS="$ROOT/tools/_restate-sync.sh"
 
 # Repository where the port shifter resides
 VERSION=v1.0.0 # Added versioning for non-breaking changes and security measures
@@ -17,10 +23,10 @@ EXTERNAL_REPO=https://raw.githubusercontent.com/zenodeapp/restate-sync/$VERSION
 
 # restate-sync.sh
 # See https://github.com/zenodeapp/restate-sync/blob/v1.0.0/restate-sync.sh
-curl -sO $EXTERNAL_REPO/restate-sync.sh
-if sh ./restate-sync.sh "$BINARY_NAME" "$NODE_DIR_NAME" "$1" "$2" "$3"; then
-    rm restate-sync.sh
+wget -O $SAVE_AS $EXTERNAL_REPO/restate-sync.sh
+if sh $SAVE_AS "$BINARY_NAME" "$NODE_DIR_NAME" "$1" "$2" "$3"; then
+    rm $SAVE_AS
 else
-    rm restate-sync.sh
+    rm $SAVE_AS
     exit 1
 fi
