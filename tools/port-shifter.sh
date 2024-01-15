ef44b78563c9598fdf050ab311b4be4c5c76bcfc
ZENODE <zenodeapp@gmail.com>
2024-01-15 11:57:16 +0100

    Update port-shifter.sh

--- tools/port-shifter.sh
+++ tools/port-shifter.sh
@@ -4,8 +4,14 @@
 # Original: https://github.com/zenodeapp/port-shifter
 # ZENODE (https://zenode.app)
 
+# Root of repository
+ROOT=$(cd "$(dirname "$0")"/.. && pwd)
+
 # Source the _variables.sh file
-. "$(cd "$(dirname "$0")"/.. && pwd)/_variables.sh"
+. "$ROOT/_variables.sh"
+
+# Where the downloaded script has to be saved.
+SAVE_AS="$ROOT/tools/_port-shifter.sh"
 
 # Repository where the port shifter resides
 VERSION=v1.0.1 # Added versioning for non-breaking changes and security measures
@@ -13,12 +19,11 @@ EXTERNAL_REPO=https://raw.githubusercontent.com/zenodeapp/port-shifter/$VERSION
 
 # quick-shift.sh (simple variant, $1 is <port_increment_value>, e.g.: 1000)
 # See https://github.com/zenodeapp/port-shifter/blob/v1.0.1/quick-shift.sh
-curl -sO $EXTERNAL_REPO/quick-shift.sh
-sh ./quick-shift.sh "$CONFIG_DIR" $1
-rm quick-shift.sh
-
+wget -O $SAVE_AS $EXTERNAL_REPO/quick-shift.sh
 # shift-wizard.sh (more advanced, if you prefer to edit individual ports)
 # See: https://github.com/zenodeapp/port-shifter/blob/v1.0.1/shift-wizard.sh
-# curl -sO $EXTERNAL_REPO/shift-wizard.sh
-# sh ./shift-wizard.sh "$CONFIG_DIR"
-# rm shift-wizard.sh
+# wget -O $SAVE_AS $EXTERNAL_REPO/shift-wizard.sh
+
+# Execute script and remove it
+sh $SAVE_AS "$CONFIG_DIR" $1
+rm $SAVE_AS
